525c4fbd9b6d7eee086fb74f011caea0c8ff9b69
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-05-26 04:47:24 +0500

    ex31

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-// create admin Array
-var userName = ["Iqra", "Afnan", "Ahmad", "Kamran", "Admin"];
-// using foreach loop
-userName.forEach(function (oneUser) {
-    if (oneUser === "Admin") {
-        console.log("Hello ".concat(oneUser, ", Would you like to see a status report? "));
-    }
-    else {
-        console.log("Hello ".concat(oneUser, ", Thank you for logging in again"));
-    }
-});
+var userNames = ["Azka", "Kamran", "Kinza", "Admin", "Ahmad"];
+userNames = [];
+// Using lenght user Name 
+if (userNames.length == 0) {
+    console.log("\n Your Array in Empty We need to find some users!\n");
+}
+else {
+    // Using ForEach loop Array
+    userNames.forEach(function (oneUser) {
+        if (oneUser === "Admin") {
+            console.log("Hello ".concat(oneUser, ", Would you like to see a status report? "));
+        }
+        else {
+            console.log("Hello ".concat(oneUser, ", Thank you for logging in again"));
+        }
+    });
+}
--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
-// create admin Array
-let userName = ["Iqra","Afnan","Ahmad","Kamran","Admin"];
- // using foreach loop
- userName.forEach(oneUser => {
-    if(oneUser === "Admin"){
-        console.log(`Hello ${oneUser}, Would you like to see a status report? `);
+let userNames = ["Azka","Kamran","Kinza","Admin","Ahmad"];
+
+userNames = []
+// Using lenght user Name 
+    if (userNames.length == 0){
+ console.log("\n Your Array in Empty We need to find some users!\n");
     }
-     else{
-        console.log(`Hello ${oneUser}, Thank you for logging in again`);
-     }
-     }
-    )
+else{
+    // Using ForEach loop Array
+    userNames.forEach(oneUser => {
+        if(oneUser === "Admin"){
+            console.log(`Hello ${oneUser}, Would you like to see a status report? `);
+        }
+         else{
+            console.log(`Hello ${oneUser}, Thank you for logging in again`);
+         }
+         }
+        )
+    }
